c3c643138487b46b5c5601f68d4facd9c56d5e4b
Francois Olivier <francoisolivier@Francoiss-Air.Dlink>
2021-08-18 20:05:28 +0200

    test

---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,5 @@
+//billInput accepts a value.
+//Whichever % value Button is selected, takes billInput value, calcualtes the % and outputs that
+ //to "Total" on screen display (Can use a <p></p> element)
+
+ 
